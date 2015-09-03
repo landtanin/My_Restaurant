1d3d4a399d169c6cd1c41f47768650997f2a1b4a
Tanin Rojanapiansatith <sp_land@hotmail.com>
2015-09-03 10:35:47 +0700

    In MyOpenHelper ->Explicit 4 things, 1 constructor with context, and extends class from SQLiteOpenHelper

---
+++ app/src/main/java/silica/landtanin/myrestaurant/MyOpenHelper.java
@@ -0,0 +1,34 @@
+package silica.landtanin.myrestaurant;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by landtanin on 9/3/15 AD.
+ */
+public class MyOpenHelper extends SQLiteOpenHelper{
+
+    // Explicit
+    private static final String DATABASE_NAME = "Restaurant.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String CREATE_USER_TABLE = "create table userTABLE (_id integer primary key, User text, Password text, Name text );";
+    private static final String CREATE_FOOD_TABLE = "create table foodTABLE (_id integer primary key, Food text, Source text, Price text)";
+
+
+    public MyOpenHelper(Context context) {
+
+
+
+    } // Constructor
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+} // Main Class
