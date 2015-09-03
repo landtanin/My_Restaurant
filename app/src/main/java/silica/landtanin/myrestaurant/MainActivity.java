a0bf6b6a534fc60cd877691d2a650c0462d6731f
Tanin Rojanapiansatith <sp_land@hotmail.com>
2015-09-03 15:02:38 +0700

    End day 1

--- app/src/main/java/silica/landtanin/myrestaurant/MainActivity.java
+++ app/src/main/java/silica/landtanin/myrestaurant/MainActivity.java
@@ -1,14 +1,27 @@
 package silica.landtanin.myrestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.InputStream;
+import java.net.HttpCookie;
 
 public class MainActivity extends AppCompatActivity {
 
     // Explicit
     private UserTABLE objUserTABLE;
+    private FoodTABLE objFoodTABLE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +32,80 @@ public class MainActivity extends AppCompatActivity {
         createAndConnectDatabase();
 
         // Tester Add Value
-        testerAddValue();
+        //testerAddValue();
 
+        // Delete All Data
+        deleteAllData();
+
+        //Synchornize JSON to SQLite
+        synJSONtoSQLite();
 
     } // onCreate
 
+    private void synJSONtoSQLite() {
+
+        //Change Policy
+        StrictMode.ThreadPolicy objPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(objPolicy);
+
+        int intTimes = 0;
+        while (intTimes <= 1) {
+
+            InputStream objInputStream = null;
+            String strJSON = null;
+            String strUserURL = "http://swiftcodingthai.com/3sep/php_get_data_land.php";
+            String strFoodURL = "http://swiftcodingthai.com/3sep/php_get_data_food.php";
+            HttpPost objHttpPost = null;
+
+            //1. Create InputStream
+            try {
+
+                HttpClient objHttpClient = new DefaultHttpClient();
+
+                if (intTimes != 1) {
+
+                    objHttpPost = new HttpPost(strUserURL);
+
+                } else {
+
+                    objHttpPost = new HttpPost(strFoodURL);
+
+                }
+
+                HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                objInputStream = objHttpEntity.getContent();
+
+            } catch (Exception e) {
+
+                Log.d("Rest", "Input ==> " + e.toString());
+
+            }
+
+            //2. Create strJSON
+
+            //3. Update to SQLite
+
+            intTimes++;
+        } // while
+
+    } // synJSONtoSQLite
+
+    private void deleteAllData() {
+
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase("Restaurant.db", MODE_PRIVATE, null);
+        objSqLiteDatabase.delete("userTABLE", null, null);
+        objSqLiteDatabase.delete("foodTABLE", null, null);
+    } // deleteAllData
+
     private void testerAddValue() {
         objUserTABLE.addNewUser("testUser", "12345", "landtanin");
+        objFoodTABLE.addFood("Fries", "test source", "200");
     } // testerAddValue
 
     private void createAndConnectDatabase() {
         objUserTABLE = new UserTABLE(this);
+        objFoodTABLE = new FoodTABLE(this);
     } // createAndConnectDatabase
 
     @Override
